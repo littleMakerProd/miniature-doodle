03b96e4a52e11a1252a5ad548f4f51ffc91acc8c
Milkent <milkentsam@gmail.com>
2016-11-13 09:42:03 +0700

    implement json response

--- skillsets/cinemas/urls.py
+++ skillsets/cinemas/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name =  'cinemas'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^movies/$', views.get_movies_today)
 ]
--- skillsets/cinemas/views.py
+++ skillsets/cinemas/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from cinemas.models import Movie
 # Create your views here.
 from django.http import HttpResponse
+from django.core import serializers
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("Cinema skill is working...")
+    return HttpResponse("You are at INDEX.") 
+
+def get_movies_today(request):
+    return HttpResponse(serializers.serialize('json',Movie.objects.all()), content_type="application/json")
