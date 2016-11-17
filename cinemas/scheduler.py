ded48600ee1347d7f50b9ee08d96d38c530ca146
Milkent <milkentsam@gmail.com>
2016-11-18 00:27:01 +0700

    add sched

--- cinemas/scheduler.py
+++ cinemas/scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minutes=28)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
