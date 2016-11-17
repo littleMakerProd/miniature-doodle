b978ba9a308a313a8470bdc3dd944234698e7cd8
Milkent <milkentsam@gmail.com>
2016-11-18 00:30:15 +0700

    change param name in cron

--- cinemas/scheduler.py
+++ cinemas/scheduler.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minutes=28)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=32)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
 
