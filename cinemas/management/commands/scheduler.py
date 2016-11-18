a45aca6a320202375e1fe8e328351663fbc40d8a
Milkent <milkentsam@gmail.com>
2016-11-19 02:08:47 +0700

    import cinema.cineplex

--- cinemas/management/commands/scheduler.py
+++ cinemas/management/commands/scheduler.py
@@ -1,11 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management.base import BaseCommand, CommandError
-import cineplex
+import cinemas.cineplex
 
 class Command(BaseCommand):
     sched = BlockingScheduler()
     
-    @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=7)
+    @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=10)
     def scheduled_job():
         get_movies_playing_now()
         print('This job is run every weekday at 5pm.')
