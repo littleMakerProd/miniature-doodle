3032af6b3cfed49ba3982dee7806d5c1e377f8fb
Milkent <milkentsam@gmail.com>
2016-11-19 02:15:03 +0700

    fix sched

--- cinemas/management/commands/scheduler.py
+++ cinemas/management/commands/scheduler.py
@@ -2,8 +2,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management.base import BaseCommand, CommandError
 import cinemas.cineplex
 
+sched = BlockingScheduler()
+
 class Command(BaseCommand):
-    sched = BlockingScheduler()
     
     @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=10)
     def scheduled_job():
