5d1e513da6067f41f3eed0c668469865a7c6d0da
Milkent <milkentsam@gmail.com>
2016-11-19 02:15:43 +0700

    fix sched

--- cinemas/management/commands/scheduler.py
+++ cinemas/management/commands/scheduler.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 
 class Command(BaseCommand):
     
-    @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=10)
+    @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=18)
     def scheduled_job():
         get_movies_playing_now()
         print('This job is run every weekday at 5pm.')
