c3024f1486abef91eb7e27e3162c98946c2fc67d
Milkent <milkentsam@gmail.com>
2016-11-20 20:09:38 +0700

    fix migration file. update cron job

--- cinemas/management/commands/scheduler.py
+++ cinemas/management/commands/scheduler.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 
 class Command(BaseCommand):
     
-    @sched.scheduled_job('cron', day_of_week='mon-sun', hour=19, minute=31)
+    @sched.scheduled_job('cron', day_of_week='mon-sun', hour=20, minute=12)
     def scheduled_job():
         cinemas.cineplex.find_showtime_in_jakarta_cinemas()
         print('This job is run every day at 6:12pm.')
